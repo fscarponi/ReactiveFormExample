5898413757f4d1a51722681bdff2cbeae8906bb9
fscarponi <fscarponi@gmail.com>
2021-03-05 15:53:26 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,11 @@ export class AppComponent {
 
   // businessHours: { [key: string]: OpeningRule[] } | undefined;
 
-  dailyOpenings: { [key: string]: ResultOfValidation };
+  dailyOpenings: { [key: string]: ResultOfValidation } = { };
 
-  dayOfWeekKeysOrdered = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'];
+  areAllFormValidated: boolean;
 
-  constructor(private fb: FormBuilder) {
-  }
-
+  daysOfWeekKeysOrdered = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'];
 
   createjson(value: any): void {
     console.log(value);
@@ -28,15 +26,26 @@ export class AppComponent {
 
   }
 
-  handleEvent(eventNotification: any): void {
-    console.log(eventNotification);
-    const result = eventNotification as ResultOfValidation;
-    if (result.error) {
-      console.log('giorno ' + result.day + ' campi non validi');
-    } else {
-      console.log('giorno ' + result.day + ' campi ok: ' + result.data);
-      this.dailyOpenings[result.day] = result;
+  handleEvent(result: ResultOfValidation): void {
+    console.log(result);
+    this.dailyOpenings[result.day] = result;
+    // now i should check validation on the full map for enable submit button
+    this.checkAllFormsAreValid();
+  }
+
+
+  // if just one form contains an error we set the areAllFormValidated as false
+  // if all ResultOfValidation are ok we set areAllFormValidated as true to enable "submit" button
+  private checkAllFormsAreValid(): void {
+    for (const day of this.daysOfWeekKeysOrdered) {
+      if (this.dailyOpenings[day] && this.dailyOpenings[day].error) {
+        console.log('Day ' + day + 'is not valid for submit');
+        this.areAllFormValidated = false;
+        return;
+      }
     }
+    this.areAllFormValidated = true;
+    console.log('All day openings are valid, here the structure ' + this.dailyOpenings);
   }
 }
 
